a9af4ab6f229b0cb9f0a37e647c5e3e34916abc4
Hamza El Alaoui <helalaou@cs.cmu.edu>
2026-08-21 18:17:10 -0400

    feat(ui): add accessible StillDue wordmark component

---
+++ src/components/Brand.tsx
@@ -0,0 +1,1 @@
+export function Brand({compact=false}:{compact?:boolean}) { return <span className="brand"><img src="/brand/mark.svg" alt="" width="38" height="38"/>{!compact&&<span>Still<span className="brand-light">Due</span><span className="brand-period">.</span></span>}</span> }
